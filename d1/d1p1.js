5621f8456076fefbc33d2f33e7466cd5c37a3f15
DipenK <45686976+dipenk73@users.noreply.github.com>
2018-12-10 11:35:11 +0000

    Update d1p1.js

--- d1/d1p1.js
+++ d1/d1p1.js
@@ -1,6 +1,9 @@
 var fs = require("fs");
 var array = fs.readFileSync("./d1_input.txt", "utf-8").toString().split('\n');
 
+// var text = fs.readFileSync("./mytext.txt").toString('utf-8');
+// var textByLine = text.split("\n")
+
 //array.forEach(function(part, index) {
 //    this[index] = this[index].replace('+','');
 //  }, array); 
